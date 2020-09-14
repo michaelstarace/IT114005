e65e6100cd007b996e129cac7a582040d4dbe9f2
michaelstarace <70656825+michaelstarace@users.noreply.github.com>
2020-09-14 19:57:13 -0400

    Add Homework 1 File

---
+++ Homework.java
@@ -0,0 +1,14 @@
+
+public class Homework {
+	public static void main(String[] args) {
+		
+		String name = "Michael";
+		System.out.println(name);
+		
+		int age = 23;
+		System.out.println(age);
+		
+		double dec = 0.123;
+		System.out.println(dec);
+	}
+}
